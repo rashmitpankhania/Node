88fcbeda38b8d8f590fdb9c566073f5b75eb677f
Rashmit Pankhania <RashmitPankhania@gmail.com>
2018-03-24 11:16:44 +0530

    just commiting

--- mad.js
+++
@@ -1,3 +0,0 @@
-var movies = require('./movie');
-movies.favMovie = '';
-console.log(` mad's favorite is ${movies.favMovie}`);
--- rash.js
+++
@@ -1,3 +0,0 @@
-var movies = require('./movie');
-movies.favMovie = 'Intersteller';
-console.log(` rashmit's favorite is ${movies.favMovie}`);
